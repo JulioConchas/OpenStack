73c218d1252491a563b4687b10f13e8eb092b570
JulioConchas <conchasjimenez@gmail.com>
2016-10-26 00:26:39 +0000

    some changes added to installer.sh and index.html

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 apt-get install -y apache2
-cd /var/www/
-pwd
-echo "-----------------------"
-ls
-echo "-----------------------"
+cd /var/www/html
+#pwd
+#echo "-----------------------"
+#ls
+#echo "-----------------------"
 wget https://raw.githubusercontent.com/JulioConchas/OpenStack/master/index.html 
 
