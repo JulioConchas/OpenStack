266a9238c7181415bd5235f1b0eca12eb2fe1712
JulioConchas <conchasjimenez@gmail.com>
2016-10-27 18:30:18 +0000

    some changes added to installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
+echo "##### installing apache #####"
+
 apt-get install -y apache2
+
+echo "##### apache installed #####"
+
+echo "##### downloadind web microservice #####"
+
 cd /var/www/html
 rm index.html
 wget https://raw.githubusercontent.com/JulioConchas/OpenStack/master/index.html 
-apt-get install mysql-server
+
+echo "##### web microservice downloaded #####"
+
+echo "##### installing mysql-server #####"
+
+apt-get install -y mysql-server
 mysql --version
+
+echo "##### mysql-server installed #####"
